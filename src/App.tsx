7a9e7f3f6a05265aa3ef70ff0bde11a17f7bb381
devpabloedu <vilelapege1113@gmail.com>
2024-11-27 20:20:09 -0300

    Configurando o ts e o react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-Dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Teste</h1>
+    </div>
+  );
+}
+
+export default App;
