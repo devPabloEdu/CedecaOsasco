765998ee2ece6dbd8cbfae4da1542295f5b81a97
devpabloedu <vilelapege1113@gmail.com>
2024-12-11 00:26:20 -0300

    Homepage finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Components/Navbar.tsx";
 import Banner from "./Components/Banner.tsx"
 import Quemsomos from "./Components/Quemsomos.tsx";
 import Ondenosencontrar from "./Components/Ondenosencontrar.tsx";
+import Timeline from "./Components/Timeline.tsx";
+import Footer from "./Components/Footer.tsx";
 import "./Styles/Navbar.css";
 import "./Styles/Banner.css";
 import "./Styles/Quemsomos.css";
 import "./Styles/Ondenosencontrar.css";
+import "./Styles/Timeline.css";
+import "./Styles/Footer.css";
+import AOS from "aos";
 
 
 
 function App() {
+
   return (
     <div className="App">
       <header>
@@ -20,7 +26,11 @@ function App() {
       </header>
       <main>
         <Ondenosencontrar />
+        <Timeline />
       </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,17 +3,22 @@ import Logocedeca from "../Assets/Logocedeca.png";
 
 function Navbar() {
     return <div>
-        <div className="navbarbox">
+        <div className="navbarbox" id="iniciodapaginaid">
             <div className="logocedecanav">
                 <img src={Logocedeca} alt="Logo Cedeca" />
             </div>
             <div className="navbarItens">
                 <a href="#Quemsomosid">Quem somos</a>
                 <a href="#Ondenosencontrarid">Onde nos encontrar</a>
+                <a href="#linhadotempoid">Linha do Tempo</a>
                 <a href="">Nossa Atuação</a>
                 <a href="">Doe</a>
                 <a href="">Redes Sociais</a>
             </div>
+        </div>
+
+        <div className="voltaroaotopobutton">
+            <a href="#iniciodapaginaid"><button>^</button></a>
         </div>
     </div>
 }
